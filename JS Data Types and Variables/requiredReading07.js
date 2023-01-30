299948c973415d70750c6b16ee238eb685c83c1d
Miroslav Ivanov <znaka5@abv.bg>
2023-01-30 13:35:05 +0200

    07 08 09 10

---
+++ JS Data Types and Variables/requiredReading07.js
@@ -0,0 +1,12 @@
+function reading(pageNumber,hourpages,days){
+
+    let totalTime=pageNumber/hourpages;
+    let requiredHours=totalTime/days;
+
+    console.log(requiredHours);
+
+
+
+}
+reading(212,20,2)
+reading(432,15 ,4 )
