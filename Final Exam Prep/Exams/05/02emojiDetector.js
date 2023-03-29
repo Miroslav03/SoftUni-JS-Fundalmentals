4af81313489c8ab8ec695155e32107c75b4d6d91
Miroslav Ivanov <znaka5@abv.bg>
2023-03-30 02:25:46 +0300

    changes

--- Final Exam Prep/Exams/05/02emojiDetector.js
+++ Final Exam Prep/Exams/05/02emojiDetector.js
@@ -1,49 +1,51 @@
 function emojiDetector(array){
 
-    let pattern = /([::|**]{2})[A-Z]([a-z]{2,})\1/g
-    let symbols = /[::|**]/g
+    function multiply (array) {
+        let sum=1;
+        for (let i=0; i<array.length; i++) {
+            sum = sum * array[i];
+        } 
+        return sum;
+    }
+
+    let pattern = /(:{2}|\*{2})(?<values>[A-Z][a-z]{2,})(\1)/g
     let numberPattern = /\d/g
 
     let text = array.shift()
-    let numbers = numberPattern.exec(text)
-    
-    let coolThreshhold = 1//
-    let emojiFound = 0 
-    let foundEmojis = []
-    let coolEmojis = []
-
-    while (numbers) {
-        let number = Number(numbers[0])
-        coolThreshhold *= number
-        numbers = numberPattern.exec(text)
-
+    let coolThreshhold = 0
+    let count = 0
+    let numbers = text. match(numberPattern)//calculating threshhold
+    if(numbers == null){
+        coolThreshhold = 0
+    }else{
+        numbers = numbers.map(Number)
+        coolThreshhold = multiply(numbers)
     }
 
-    let emojis = pattern.exec(text)
-
-    while (emojis) {
-        let emoji = emojis[0]
-        foundEmojis.push(emoji)
-        emojis = pattern.exec(text)
-    }
-
-    emojiFound = foundEmojis.length
-
-    for (let i = 0; i < foundEmojis.length; i++) {
-        let element = foundEmojis[i]
-        let finalCode = 0
-        for (let j = 2; j < element.length-2; j++) {
-            let letter = element[j]
-            let code = letter.charCodeAt()
-            finalCode +=code
-        }
-        if(finalCode>coolThreshhold){
-            coolEmojis.push(element)
+    let validEmojis = text.match(pattern)
+    coolEmojis = []
+    if(validEmojis == null){
+        count = 0
+        coolEmojis = []
+    }else{
+        count = validEmojis.length
+        for (let i = 0; i < validEmojis.length; i++) {
+            let element = validEmojis[i]
+            let finalCode = 0
+            for (let j = 2; j < element.length-2; j++) {
+                let letter = element[j]
+                let code = letter.charCodeAt()
+                finalCode +=code
+            }
+            if(finalCode>coolThreshhold){
+                coolEmojis.push(element)
+            }
         }
     }
+
 
     console.log(`Cool threshold: ${coolThreshhold}`);
-    console.log(`${emojiFound} emojis found in the text. The cool ones are:`);
+    console.log(`${count} emojis found in the text. The cool ones are:`);
     console.log(coolEmojis.join(`\n`));
 
-}emojiDetector(["It is a long established fact that 1 a reader will be distracted by 9 the readable content of a page when looking at its layout. The point of using ::LoremIpsum:: is that it has a more-or-less normal 3 distribution of 8 letters, as opposed to using 'Content here, content 99 here', making it look like readable **English**."])
+}emojiDetector([":*Inavlid:*"])
